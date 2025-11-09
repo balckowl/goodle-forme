1448c92ea00bda36976b9fdf49d47b6b5c985fbf
balckowl <shiggyjr1492@gmail.com>
2025-11-09 21:51:24 +0900

    fix

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,17 +1,17 @@
-import type { FeatureFlags } from "@/server/schemas/admin.schema";
 import AdminWrapper from "../components/AdminWrapper";
+import { hono } from "../lib/hono-client";
 
 export default async function Page() {
-  // const res = await hono.api.allFlags.$get();
-  // const flags = await res.json();
+  const res = await hono.api.allFlags.$get();
+  const flags = await res.json();
 
-  const flags: FeatureFlags = {
-    isFormeBoldness: true,
-    isFormeCreativity: true,
-    isFormeExecution: true,
-    isFormeHumor: true,
-    isFormePresentation: true,
-  };
+  // const flags: FeatureFlags = {
+  //   isFormeBoldness: true,
+  //   isFormeCreativity: true,
+  //   isFormeExecution: true,
+  //   isFormeHumor: true,
+  //   isFormePresentation: true,
+  // };
 
   return <AdminWrapper flags={flags} />;
 }
--- src/app/lib/hono-client.ts
+++ src/app/lib/hono-client.ts
@@ -1,7 +1,6 @@
 import { hc } from "hono/client";
-import { env } from "@/env.mjs";
 import type { pub } from "@/server/hono";
 
 type AppType = typeof pub;
 
-export const hono = hc<AppType>(env.NEXT_PUBLIC_APP_URL);
+export const hono = hc<AppType>(process.env.NEXT_PUBLIC_APP_URL as string);
